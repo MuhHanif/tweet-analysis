b312d87c0c8c99aaa556dd039d87edf4137789d6
MuhHanif <48muhhanif@gmail.com>
2021-09-12 09:23:39 +0700

    copy paste code snippet fromm tweepy docs

--- analysis/connect.py
+++ analysis/connect.py
@@ -1,11 +1,20 @@
-import tweepy as tp;
-from textblob import TextBlob as blob;
-import pandas as pd;
-import numpy as np;
-import re;
-import matplotlib.pyplot as plt;
+import tweepy as tp
+from textblob import TextBlob as blob
+import pandas as pd
+import numpy as np
+import re
+import matplotlib.pyplot as plt
 
 
+### this is code snippet directly from tweepy docs ###
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
 
 
 
