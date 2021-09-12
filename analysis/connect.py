6c8b137f3b4216aaef7f5ab8f3af9896f64bb52c
MuhHanif <48muhhanif@gmail.com>
2021-09-12 09:15:22 +0700

    just testing my pc lib

---
+++ analysis/connect.py
@@ -0,0 +1,12 @@
+import tweepy as tp;
+from textblob import TextBlob as blob;
+import pandas as pd;
+import numpy as np;
+import re;
+import matplotlib.pyplot as plt;
+
+
+
+
+
+#something to be added hehe
