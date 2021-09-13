50c3319cb24e04c7785a136523aed0f1788df1a9
MuhHanif <48muhhanif@gmail.com>
2021-09-13 10:32:53 +0700

    create auth class

--- analysis/connect.py
+++ analysis/connect.py
@@ -1,30 +1,40 @@
-import tweepy
-from textblob import TextBlob as blob
+from tweepy import OAuthHandler
+from tweepy import API
 import csv
-#import pandas as pd
-#import numpy as np
-#import re
-#import matplotlib.pyplot as plt
+import sys
 
-#read the key from my csv files
-csvDir = input("enter csv dir: ")
+### key file detection ###
+try:
+    csvDir = sys.argv[1]
+    pass
+except:
+    print("please enter the key file as an argument")
+    print("python3 connect.py <key file>")
+    sys.exit("terminated no key detected")
+
 #hardcode key for debug only
 csvDir = "/home/gray/git-repo/twitter-key/key.tweet"
 
+#read csv key file
 with open(csvDir, mode ='r')as file:
   # reading the CSV file
   tokenList = list(csv.reader(file))
 
-### this is code snippet directly from tweepy docs ###
-auth = tweepy.OAuthHandler(tokenList[0][1], tokenList[1][1])
-auth.set_access_token(tokenList[2][1], tokenList[3][1])
+""""Authenticator class"""
+class twitterAuth():
+    """authenticate to twitter API"""
+    def authenticate(self):
+        auth = OAuthHandler(tokenList[0][1], tokenList[1][1])
+        auth.set_access_token(tokenList[2][1], tokenList[3][1])
+        return(auth)
 
-api = tweepy.API(auth)
+
+auth = twitterAuth().authenticate()
+
+api = API(auth)
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
-
-
 
 #something to be added hehe
